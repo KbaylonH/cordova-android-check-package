98f8da79a7f079847671de0707ced373654808bc
KbaylonH <kbaylonh@outlook.com>
2018-07-30 16:43:15 -0500

    fixed
    Some dependents packages added

--- src/android/com/kbaylonh/CheckPackagePlugin.java
+++ src/android/com/kbaylonh/CheckPackagePlugin.java
@@ -1,29 +1,35 @@
 package com.kbaylonh;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import java.util.List;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
+
 
 public class CheckPackagePlugin extends CordovaPlugin {
 
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView); 
+    super.initialize(cordova, webView);
   }
 
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext){
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException{
 
     if("checkApp".equals(action)){
       String packageName = args.getString(0);
       List<ApplicationInfo> packages;
       PackageManager pm;
 
-      pm = getPackageManager();        
+      pm = super.cordova.getContext().getPackageManager();
       packages = pm.getInstalledApplications(0);
       for (ApplicationInfo packageInfo : packages) {
         if(packageInfo.packageName.equals(packageName)){
